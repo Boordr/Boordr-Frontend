3685d027d9140d397c1be45ac68a7b6c5bb6e42a
Tony Heydorf <ashyiscool2@gmail.com>
2022-05-24 17:38:33 -0400

    First commit - Nuxt setup

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <NuxtLayout name="custom">
+    <template #header>  
+        <div class="container-2xl border-2 border-rose-600">
+            <div class="grid grid-cols-4">
+                <div class="border-2 col-span-3 border-green-500">
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                   <span>/a/ </span>
+                </div>
+                <div class="border-2 border-green-500">
+                    border-green-600 A YOOO
+                </div>            
+            </div>
+        </div>
+    </template>
+
+    The rest of the page
+  </NuxtLayout>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: false,
+});
+</script>
