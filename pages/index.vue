c9dc8882f9ad34cbd1702137fe30ceb99a6f49e1
Tony Heydorf <ashyiscool2@gmail.com>
2022-05-24 21:21:14 -0400

    Began work on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,136 @@
 <template>
-  <NuxtLayout name="custom">
-    <template #header>  
-        <div class="container-2xl border-2 border-rose-600">
-            <div class="grid grid-cols-4">
-                <div class="border-2 col-span-3 border-green-500">
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                   <span>/a/ </span>
-                </div>
-                <div class="border-2 border-green-500">
-                    border-green-600 A YOOO
-                </div>            
-            </div>
-        </div>
-    </template>
+    <NuxtLayout name="custom"></NuxtLayout>
 
-    The rest of the page
-  </NuxtLayout>
+    <div class="container border border-[#d0d7de] rounded drop-shadow-sm mt-4 mx-auto bg-white">
+
+        <div class="mx-auto lg:py-3 lg:px-8">
+
+            <div class="flex">
+
+                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mt-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+                    clip-rule="evenodd" />
+                </svg>
+                
+                <h2 class="text-3xl mx-1 font-extrabold tracking-tight text-indigo-600 sm:block sm:text-4xl"> Browse our popular boords.</h2>
+            
+            </div>
+
+            <!-- Boards -->
+            <div class="py-5 flex flex-wrap">
+
+                <span
+                    class="text-sm text-white hover:bg-orange-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-orange-400">Art</span>
+
+                <span
+                    class="text-sm text-white hover:bg-emerald-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-emerald-400">Anime</span>
+
+                <span
+                    class="text-sm text-white hover:bg-sky-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-sky-400">Video Games</span>
+
+                <span
+                    class="text-sm text-white hover:bg-rose-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-rose-400">Volley Ball</span>
+
+                <span
+                    class="text-sm text-white hover:bg-purple-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-purple-400">Programming</span> 
+
+                <span
+                    class="text-sm text-white hover:bg-orange-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-orange-400">Fun</span>
+
+                <span
+                    class="text-sm text-white hover:bg-emerald-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-emerald-400">Random</span>
+
+                <span
+                    class="text-sm text-white hover:bg-sky-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-sky-400">Pokemon</span>
+
+                <span
+                    class="text-sm text-white hover:bg-rose-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-rose-400">Nintendo</span>
+
+                <span
+                    class="text-sm text-white hover:bg-purple-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-purple-400">Programming</span> 
+
+                  <span
+                    class="text-sm text-white hover:bg-orange-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-orange-400">Art</span>
+
+                <span
+                    class="text-sm text-white hover:bg-emerald-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-emerald-400">Anime</span>
+
+                <span
+                    class="text-sm text-white hover:bg-sky-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-sky-400">Video Games</span>
+
+                <span
+                    class="text-sm text-white hover:bg-rose-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-rose-400">Volley Ball</span>
+
+                <span
+                    class="text-sm text-white hover:bg-purple-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-purple-400">Programming</span>
+                
+                <span
+                    class="text-sm text-white hover:bg-orange-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-orange-400">Art</span>
+
+                <span
+                    class="text-sm text-white hover:bg-emerald-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-emerald-400">Anime</span>
+
+                <span
+                    class="text-sm text-white hover:bg-sky-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-sky-400">Video Games</span>
+
+                <span
+                    class="text-sm text-white hover:bg-rose-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-rose-400">Volley Ball</span>
+
+                <span
+                    class="text-sm text-white hover:bg-purple-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-purple-400">Programming</span>
+                
+                  <span
+                    class="text-sm text-white hover:bg-orange-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-orange-400">Art</span>
+
+                <span
+                    class="text-sm text-white hover:bg-emerald-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-emerald-400">Anime</span>
+
+                <span
+                    class="text-sm text-white hover:bg-sky-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-sky-400">Video Games</span>
+
+                <span
+                    class="text-sm text-white hover:bg-rose-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-rose-400">Volley Ball</span>
+
+                <span
+                    class="text-sm text-white hover:bg-purple-700 font-medium py-3 px-3 mx-1 my-1 rounded cursor-pointer bg-purple-400">Programming</span> 
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="flex flex-wrap my-2 max-h-3">
+
+        <div>
+
+            <h3>Subject</h3>
+
+            <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/200">
+
+            <p>post | views | replies </p>
+
+        </div>
+        
+        <!-- <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/300/190">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/400/200">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/100">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/150">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/155/200">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/100/140">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/165">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/157">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/200/195">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/220/200">
+        <img class="mx-2 my-2 cursor-pointer hover:brightness-125" src="https://placewaifu.com/image/240/155"> -->
+
+    </div>
+
 </template>
 
 <script setup>
 definePageMeta({
-  layout: false,
+    layout: false,
 });
 </script>
