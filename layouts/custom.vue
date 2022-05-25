1bf8420700646c920974451d258d1a192ffc282a
Tony Heydorf <ashyiscool2@gmail.com>
2022-05-24 21:25:47 -0400

    Update to README + Packages

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
-    <header>
-      <slot name="header">
-        Default header content
-      </slot>
-    </header>
-    <main>
-      <slot />
-    </main>
-  </div>
+  <header class="bg-indigo-600">
+    <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+      <div class="w-full py-3 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+        <div class="flex items-center">
+          <a href="#">
+            <span class="sr-only">Workflow</span>
+            <img class="h-10 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white" alt="" />
+          </a>
+          <div class="hidden ml-10 space-x-8 lg:block">
+            <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">
+              {{ link.name }}
+            </a>
+          </div>
+        </div>
+        <div class="ml-10 space-x-4">
+          <a href="#" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">Sign in</a>
+        </div>
+      </div>
+      <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+        <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">
+          {{ link.name }}
+        </a>
+      </div>
+    </nav>
+  </header>
 </template>
+
+<script setup>
+const navigation = [
+  { name: 'Link', href: '#' },
+  { name: 'Link', href: '#' },
+  { name: 'Link', href: '#' },
+  { name: 'Link', href: '#' },
+]
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {},
+    plugins: [require('@tailwindcss/forms'),],
+    content: [
+      `components/**/*.{vue,js}`,
+      `layouts/**/*.vue`,
+      `pages/**/*.vue`,
+      `composables/**/*.{js,ts}`,
+      `plugins/**/*.{js,ts}`,
+      `App.{js,ts,vue}`,
+      `app.{js,ts,vue}`
+    ]
 }
